bad4a76cbc665cd90f46f7dddf39b374f94316ff
pratik-deve <pratik.devakate@wissen.com>
2025-08-11 11:13:15 +0530

    Added SecurityFilterChain and Static UserDetailsServices

--- src/main/java/com/springSecutity/Spring/Security/config/SecurityConfig.java
+++ src/main/java/com/springSecutity/Spring/Security/config/SecurityConfig.java
@@ -3,9 +3,20 @@ package com.springSecutity.Spring.Security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfToken;
+
+import java.net.http.HttpRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +25,67 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        /*
+//        http.csrf(customizer -> customizer.disable()); This without lambda looks like
+        Customizer<CsrfConfigurer<HttpSecurity>> csrfConfigurer = new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            @Override
+            public void customize(CsrfConfigurer<HttpSecurity> customizer) {
+                customizer.disable();
+            }
+        };
 
-        return http.build();
+        http.csrf(csrfConfigurer);
+
+//        http.authorizeHttpRequests(request -> request.anyRequest().authenticated() );
+
+        Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> authorizer =
+                new Customizer<AuthorizeHttpRequestsConfigurer<org.springframework.security.config.annotation.web.builders.HttpSecurity>.AuthorizationManagerRequestMatcherRegistry>() {
+            @Override
+            public void customize(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authRegistry) {
+                authRegistry.anyRequest().authenticated();
+            }
+        };
+
+        http.authorizeHttpRequests(authorizer);
+
+
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+*/
+
+        //above can be written as http streams also
+
+        return http
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(customizer -> customizer.disable())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("rahul")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User
+                .withDefaultPasswordEncoder()
+                .username("admin")
+                .password("Admin@123")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+
     }
 }
